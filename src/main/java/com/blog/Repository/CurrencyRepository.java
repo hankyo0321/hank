90d6ae986a6625e4c836c98f4d19202277fe2a4c
hank <hank@lingenghandembp.lan>
2023-12-27 19:05:25 +0800

    hankblog

--- src/main/java/com/blog/Repository/CurrencyRepository.java
+++ src/main/java/com/blog/Repository/CurrencyRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blog.pojo.Currency;
 
-public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
+public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 	
 	
 	Currency findByCode(String code);
