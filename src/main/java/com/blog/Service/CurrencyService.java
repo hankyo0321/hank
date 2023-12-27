18b557c5951f3e4263e4bce54e4a65d3e6161204
hank <hank@lingenghandembp.lan>
2023-12-27 16:58:24 +0800

    hankblog

---
+++ src/main/java/com/blog/Service/CurrencyService.java
@@ -0,0 +1,35 @@
+package com.blog.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.blog.Repository.CurrencyRepository;
+import com.blog.pojo.Currency;
+
+import java.util.List;
+
+@Service
+public class CurrencyService {
+
+    @Autowired
+    private CurrencyRepository currencyRepository;
+
+    public List<Currency> getAllCurrencies() {
+        return currencyRepository.findAll();
+    }
+
+    public Currency getCurrencyByCode(String code) {
+        return currencyRepository.findByCode(code);
+    }
+
+    public Currency addCurrency(Currency currency) {
+        return currencyRepository.save(currency);
+    }
+
+    public Currency updateCurrency(Currency currency) {
+        return currencyRepository.save(currency);
+    }
+
+    public void deleteCurrency(int id) {
+        currencyRepository.deleteById(id);
+    }
+}
